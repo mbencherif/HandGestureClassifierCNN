be3a492a6847bd2045852d765c0975799a68b927
Yuhan Liu <yliu1021@hotmail.com>
2019-06-06 03:47:40 -0700

    Better map

--- process_data.py
+++ process_data.py
@@ -70,16 +70,18 @@ def resize_images(home_path="/home/yliu102199"):
 
     with Pool(16) as pool:
         print("Resizing training images")
-        for i, _ in enumerate(pool.map(resize_folder, feature_labels, 16)):
-            sys.stdout.write('\rDone... {0:%}'.format(i / len(feature_labels)))
-        sys.stdout.write('\n')
-        sys.stdout.flush()
+        pool.map(resize_folder, feature_labels, 16)
+        # for i, _ in enumerate(pool.map(resize_folder, feature_labels, 16)):
+        #     sys.stdout.write('\rDone... {0:%}'.format(i / len(feature_labels)))
+        # sys.stdout.write('\n')
+        # sys.stdout.flush()
 
         print("Resizing validation images")
-        for i, _ in enumerate(pool.map(resize_folder, validation_feature_labels, 16)):
-            sys.stdout.write('\rDone... {0:%}'.format(i / len(validation_feature_labels)))
-        sys.stdout.write('\n')
-        sys.stdout.flush()
+        pool.map(resize_folder, validation_feature_labels, 16)
+        # for i, _ in enumerate(pool.map(resize_folder, validation_feature_labels, 16)):
+        #     sys.stdout.write('\rDone... {0:%}'.format(i / len(validation_feature_labels)))
+        # sys.stdout.write('\n')
+        # sys.stdout.flush()
 
         # print("Resizing test images")
         # for i, _ in enumerate(pool.imap_unordered(resize_folder, test_feature_labels)):
