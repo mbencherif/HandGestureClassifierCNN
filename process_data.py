d4312935da1e799ea28f6f60be2d565732f6c623
Yuhan Liu <yliu1021@hotmail.com>
2019-06-06 17:18:40 -0700

    Single process

--- process_data.py
+++ process_data.py
@@ -68,28 +68,27 @@ def resize_images(home_path="/home/yliu102199"):
     #         folder_name, _ = line.split(";", maxsplit=2)
     #         test_feature_labels.append(os.path.join(dataset_base, folder_name))
 
-    with Pool(16) as pool:
-        print("Resizing training images")
-        # for i, _ in enumerate(pool.map(resize_folder, feature_labels, 16)):
-        for i, _ in enumerate(map(resize_folder, feature_labels)):
-            sys.stdout.write('\rDone... {0:%}'.format(i / len(feature_labels)))
-        sys.stdout.write('\n')
-        sys.stdout.flush()
+    print("Resizing training images")
+    # for i, _ in enumerate(pool.map(resize_folder, feature_labels, 16)):
+    for i, _ in enumerate(map(resize_folder, feature_labels)):
+        sys.stdout.write('\rDone... {0:%}'.format(i / len(feature_labels)))
+    sys.stdout.write('\n')
+    sys.stdout.flush()
 
-        print("Resizing validation images")
-        # for i, _ in enumerate(pool.map(resize_folder, validation_feature_labels, 16)):
-        for i, _ in enumerate(map(resize_folder, validation_feature_labels)):
-            sys.stdout.write('\rDone... {0:%}'.format(i / len(validation_feature_labels)))
-        sys.stdout.write('\n')
-        sys.stdout.flush()
+    print("Resizing validation images")
+    # for i, _ in enumerate(pool.map(resize_folder, validation_feature_labels, 16)):
+    for i, _ in enumerate(map(resize_folder, validation_feature_labels)):
+        sys.stdout.write('\rDone... {0:%}'.format(i / len(validation_feature_labels)))
+    sys.stdout.write('\n')
+    sys.stdout.flush()
 
-        # print("Resizing test images")
-        # for i, _ in enumerate(pool.imap_unordered(resize_folder, test_feature_labels)):
-        #     sys.stdout.write('\rDone... {0:%}'.format(i / len(test_feature_labels)))
-        # sys.stdout.write('\n')
-        # sys.stdout.flush()
+    # print("Resizing test images")
+    # for i, _ in enumerate(pool.imap_unordered(resize_folder, test_feature_labels)):
+    #     sys.stdout.write('\rDone... {0:%}'.format(i / len(test_feature_labels)))
+    # sys.stdout.write('\n')
+    # sys.stdout.flush()
 
-        print("Done")
+    print("Done")
 
 
 if __name__ == '__main__':
