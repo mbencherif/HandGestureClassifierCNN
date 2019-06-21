2ea685fb808e29adf3a5f9303b1c4c5bcdd9f70c
Yuhan Liu <yliu1021@hotmail.com>
2019-06-21 00:04:35 -0400

    Increased learning rate

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ def start_training(data_location='/Users/Yuhan', log_dir='log1', save_dir='saved
     loss_op = get_loss(model_output, y_actual_placeholder)
 
     print('creating optimizers')
-    optimizer = get_optimizer(loss_op, lr=0.001, opt='adam')
+    optimizer = get_optimizer(loss_op, lr=0.01, opt='adam')
 
     print('creating metrics')
     metric_calc_ops = list()
